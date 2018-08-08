1edadfe7f8d4a2821f1ed16e923e81a4d8af31df
oliviatian <oliviatian27@gmail.com>
2018-08-07 23:13:21 -0400

    done

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+class Song
+  @@count=0
+  @@artists=[]
+  @@genres=[]
+  @@genre_count={}
+  @@artist_count={}
+  attr_accessor :name,:artist,:genre
+  def initialize(name,artist,genre)
+    @name=name
+    @artist = artist
+    @genre=genre
+    @@count+=1
+     @@artists<<artist
+     @@genres<<genre
+
+      if @@genre_count.has_key?(genre)
+        @@genre_count[genre]+=1
+      else
+        @@genre_count[genre]=1
+      end
+      if @@artist_count.has_key?(artist)
+        @@artist_count[artist]+=1
+      else
+        @@artist_count[artist]=1
+      end
+    end
+
+  def self.count
+     @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genre_count
+  end
+
+  def self.artist_count
+    @@artist_count
+  end
+end
